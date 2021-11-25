345706fab7e2505f85d36afb41c7537e25aa3e1e
Jonathan McDermid <jonathan.mcdermid1@ucalgary.ca>
2021-11-24 23:10:37 -0700

    added basic decision tree file

--- dt.py
+++ dt.py
@@ -4,6 +4,7 @@ import log_parser
 import numpy as np
 import pandas as pd
 import math
+from sklearn import tree
 
 # 10 second time segments
 segment_time = 10
@@ -16,8 +17,10 @@ keyboard_data = log_parser.parse_keyboard_log("keyboard_actions.log")
 
 segment_list = []
 
+# Causes crash for some reason
+
 for i in range(nsegments):
-    keyboard_data_segment = log_parser.get_segment(keyboard_data, i, segment_time)
+    keyboard_data_segment = log_parser.get_segment(keyboard_data, i + 1, segment_time)
     segment_list.append(keyboard_heatmap.keyboardHeatmap.parseKeyboardLog(keyboard_data_segment))
 
 print(segment_list)
