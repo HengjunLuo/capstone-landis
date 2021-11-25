0f974ef02e414c49748a9d987c8fc6886d5d859d
Joseph Cordara <josephcordara@gmail.com>
2021-11-25 01:42:17 -0700

    Move keyboard feature extraction to log_parser.py

--- keyboard_heatmap.py
+++ keyboard_heatmap.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+import log_parser
+
 class KeyboardHeatmap:
 
     def __init__(self, dataframe):
@@ -13,7 +15,7 @@ class KeyboardHeatmap:
                     "t","Key.tab","Key.f5","Key.f6","Key.f7","`","j","k"]
 
         # Extract frequency and duration data from segment
-        self.keyboard_df = parseKeyboardLog(dataframe)
+        self.keyboard_df = log_parser.extract_keyboard_features(dataframe)
 
         # Set key column to index (unique values)
         self.keyboard_df.set_index('key', inplace=True)
@@ -66,81 +68,3 @@ class KeyboardHeatmap:
 
         # Show heatmaps for frequency and average duartion pressed for keys
         plt.show()
-
-
-def parseKeyboardLog(dataframe):
-    # The 49 default key bindings for team fortress 2
-    keyBindings = ["w","a","s","d","Key.space","Key.ctrl_l","'","/","Key.up","Key.down",
-                "v","y","u","z","x","c",",",".","m","n","Key.f2","Key.f3","l","g",
-                "h","i","f","b","-","r","q","1","2","3","4","5","6","7","8","9","0",
-                "t","Key.tab","Key.f5","Key.f6","Key.f7","`","j","k"]
-
-    resultDict = {} # containing 'key':[last press time, last action, total duration, number of key strokes]
-
-    #Calculate duration and freq for each key between startMin and endMin
-    for _, row in dataframe.iterrows():
-        # Extract keyname (remove surrounding '' if needed)
-        key = row['key']
-        key = key.replace("'", "")
-
-        if key in keyBindings:
-            # Get the time of the action
-            time = float(row['time'])
-            action = row['action']
-
-            if key in resultDict.keys():
-                # If this key's last action was 'pressed'
-                if resultDict[key][1] == "pressed":
-                    if action == "released": # Ensure that this key's current action is 'released'
-                        # Record that this key's last action was 'released'
-                        resultDict[key][1] = action
-                        # Add the duration of this key's press (duration = now - time of last press)
-                        resultDict[key][2] += time - resultDict[key][0]
-                        # Increment total number of keypresses for this key
-                        resultDict[key][3] += 1
-
-                else: # If last action was not 'pressed'
-                    if action == "pressed": # Ensure that this action is 'pressed'
-                        # Record time of this key press
-                        resultDict[key][0] = time
-                        # Record that this key's last action was 'pressed'
-                        resultDict[key][1] = action
-
-            else: # If the key hasn't been processed yet, add it to the dictionary
-                if action == "pressed": # Only if the key's first action is 'pressed'
-                    # Record when the key was pressed and nitialize duration and press count
-                    resultDict[key] = [time, action, 0, 0]
-
-
-    # Default values if log is empty
-    classID = "NaN"
-    seg_length = 0
-
-    if len(dataframe.index) > 0: # If the dataframe isnt empty
-        # Calculate segment duration
-        seg_length = dataframe.time.max() - dataframe.time.min()
-        # Infer class label from first line
-        classID = dataframe.iloc[0]["class"] 
-
-    # Build Pandas DataFrame from results
-    resultList = [] # Empty 2D array
-    for key, values in resultDict.items():
-        avg_duration = values[2] / max(values[3], 1) # Average duration = total duration / # key presses
-        freq = values[3] / max(seg_length, 1) # Frequency = # key presses / segment length
-
-        # Format the entry in the DataFrame
-        sublist =[key, str("{:.3f}".format(avg_duration)), str("{:.3f}".format(freq)), classID]
-        resultList.append(sublist)
-
-    # Return a Pandas DataFrame built from results
-    return pd.DataFrame(resultList, columns = ['key', 'avg_duration', 'freq', 'class'])
-
-
-"""
-import log_parser
-
-keyboard = log_parser.parse_keyboard_log("logs/joseph/keyboard.log")
-keyboard_seg = log_parser.get_segment(keyboard, 1, seg_length=500)
-
-parseKeyboardLog(keyboard_seg)
-"""
