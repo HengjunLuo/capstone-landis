0f3ee8e6dd98992184ba6ed2ff1294ef247454d6
Jonathan McDermid <jonathan.mcdermid1@ucalgary.ca>
2021-11-24 22:32:39 -0700

    changed decision tree file to .py for debugging

---
+++ dt.py
@@ -0,0 +1,23 @@
+# Import keyboard_heatmap module
+import keyboard_heatmap
+import log_parser
+import numpy as np
+import pandas as pd
+import math
+
+# 10 second time segments
+segment_time = 10
+
+# Getting the data from  parsed input keyboard log file
+keyboard_data = log_parser.parse_keyboard_log("keyboard_actions.log")
+nsegments = math.trunc(keyboard_data.iloc[-1]['time'] / segment_time)
+
+keyboard_data = log_parser.parse_keyboard_log("keyboard_actions.log")
+
+segment_list = []
+
+for i in range(nsegments):
+    keyboard_data_segment = log_parser.get_segment(keyboard_data, i, segment_time)
+    segment_list.append(keyboard_heatmap.keyboardHeatmap.parseKeyboardLog(keyboard_data_segment))
+
+print(segment_list)
